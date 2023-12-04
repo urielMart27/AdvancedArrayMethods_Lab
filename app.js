f942a076355f109f2560e9e63368aee2ee9254c1
Uriel Martinez <martinezuriel651@gmail.com>
2023-12-04 17:09:24 -0600

    Finished Problems Nine and Ten

--- app.js
+++ app.js
@@ -272,10 +272,32 @@ console.log("Cuisine Types:", cuisineTypes);
 //9. Create a function that will return an array of strings, with the cuisine type appended to the start of the dish's name. Ie, ["Italian Pizza", "Italian Spaghetti", ...]
 //Map 
 
+function combineNames(){
+    let results = dishes.map(function(el){
+        return el.cuisine + " " + el.name;
+    });
+    return results;
+}
+
+let combinednames = combineNames();
+console.log("Combined Names:", combinednames);
 
 
 //10. Create a function that will use advanced array methods on the 'dishes' array and return the result ["Vegetarian Lasagna", "Vegetarian Falafel", "Vegetarian Chili"]
 
+function getvegetarianDishes(){
+    let results = dishes.filter(function(el){
+        return el.cuisine.toLowerCase() === "vegetarian"
+    });
+
+    let combinedVegetarianDishNames = results.map(function(el) {
+        return el.cuisine + " " + el.name;
+    });
+    return combinedVegetarianDishNames;
+}
+
+let vegetarianDishes = getvegetarianDishes();
+console.log("Vegetarian Dishes:", vegetarianDishes);
 
 
 
@@ -283,6 +305,7 @@ console.log("Cuisine Types:", cuisineTypes);
 //BONUS
 
 //8b. Use the filter method to eliminate duplicate from problem 8a.
+
 
 //11. Create a function that will return dishes whose ingredients array INCLUDES "tomato" OR "cheese".
 //Hint: You do not want to check the array's indexes to find out what the array INCLUDES.
